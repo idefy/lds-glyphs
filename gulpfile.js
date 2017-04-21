d9054bb9eb3121f412e2d53f3d74c475e3bb283a
Igor DeFaye <idefaye@gmail.com>
2017-04-21 12:24:43 -0400

    minor fixes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs');
 var path = require('path');
-//var args = require('yargs').argv;
+var args = require('yargs').argv;
 var del = require('del');
 var browserSync = require('browser-sync');
 var config = require('./gulp.config')();
@@ -87,6 +87,45 @@ gulp.task('clean', function() {
     var delconfig = [].concat(config.dist, config.temp);
     //log('Cleaning: ' + $.util.colors.blue(delconfig));
     del(delconfig);
+});
+
+gulp.task('bump', function () {
+    /// <summary>
+    /// It bumps revisions
+    /// Usage:
+    /// 1. gulp bump : bumps the package.json and bower.json to the next minor revision.
+    ///   i.e. from 0.1.1 to 0.1.2
+    /// 2. gulp bump --version 1.1.1 : bumps/sets the package.json and bower.json to the 
+    ///    specified revision.
+    /// 3. gulp bump --type major       : bumps 1.0.0 
+    ///    gulp bump --type minor       : bumps 0.1.0
+    ///    gulp bump --type patch       : bumps 0.0.2
+    ///    gulp bump --type prerelease  : bumps 0.0.1-2
+    /// </summary>
+	var msg = "";
+    var type = args.type;
+    var version = args.version;
+    var options = {};
+
+    if (version) {
+        options.version = version;
+        msg += ' to ' + version;
+    } else {
+        options.type = type;
+		if(type) {
+			msg += ' for a ' + type;
+		} else {
+			msg += ' for a patch'; 
+		}
+    }
+
+    return gulp
+        .src(['package.json', 'bower.json'])
+		.pipe($.tap(function(file) {
+			log("Bumping " + path.basename(file.path) + msg + ":");
+		}))
+        .pipe($.bump(options))
+        .pipe(gulp.dest('.'));
 });
 
 /////////////// functions /////////////////////////
